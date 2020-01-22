db4fd6a270adbf7c6e62c46243754be6fbe6d262
RohitNethi <snethi@uncc.edu>
2020-01-21 22:46:06 -0500

    add aws amplify and front end code

---
+++ src/config.js
@@ -0,0 +1,16 @@
+export default {
+  s3: {
+    REGION: "us-east-1",
+    BUCKET: "notes-app-v1-uploads"
+  },
+  apiGateway: {
+    REGION: "us-east-1",
+    URL: "https://751oig80uh.execute-api.us-east-1.amazonaws.com/prod"
+  },
+  cognito: {
+    REGION: "us-east-1",
+    USER_POOL_ID: "us-east-1_uuuVEiSqq",
+    APP_CLIENT_ID: "5oj6nqslfg6movp49fa8il1dkg",
+    IDENTITY_POOL_ID: "us-east-1:6466ffce-2954-42f3-8c83-52923b3febe6"
+  }
+};
